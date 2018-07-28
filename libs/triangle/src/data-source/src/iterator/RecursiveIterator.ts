b57bb9bcc2b89835ec652ef14af2a81673e3df6a
Linbo Len <LinboLen@Gradee.org>
2018-07-28 16:37:14 +0800

    [WIP] feat: data source, working on iterator

---
+++ libs/triangle/src/data-source/src/iterator/RecursiveIterator.ts
@@ -0,0 +1,36 @@
+export class RecursiveIterator {
+  protected stackedIter: RecursiveIterator[];
+
+  constructor(protected it: RecursiveIterator) {
+  }
+
+  public getChildren() {
+
+  }
+
+  public hasChildren() {
+
+  }
+
+  public next() {
+    if (this.it) {
+      const next = this.it.next();
+      if (next) {
+        if (this.it.current().hasChildren()) {
+          this.stackedIter.push(this.it);
+          this.it = this.it.getChildren();
+        }
+        return true;
+      } else if(this.stackedIter.length > 0) {
+        this.it = this.stackedIter.pop();
+        return this.next();
+      } else {
+        return false;
+      }
+    }
+  }
+
+  public current() {
+    this.it.current();
+  }
+}
